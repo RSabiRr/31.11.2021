ce95773e202e21563d44df47259cad9d4ff2f5c4
sabir <tu201806274@code.edu.az>
2021-11-03 23:53:44 +0300

    sfasf

---
+++ Access modifiers, Namespace, Library/Access modifiers, Namespace, Library/Program.cs
@@ -0,0 +1,22 @@
+﻿using Library1;
+using System;
+
+namespace Access_modifiers__Namespace__Library
+{
+    class Program :Class1
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 class1 = new Class1();
+            class1.a = 3;
+            class1.b = 4;
+
+            Console.WriteLine(Class1.toplama(3,4));
+
+            Console.WriteLine(Class1.vurma(class1.a, class1.b));
+        }
+
+       
+    }
+}
---
+++ Access modifiers, Namespace, Library/Library1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Library1
+{
+    public class Class1
+    {
+        public int a;
+        public int b;
+        public static int toplama(int a, int b)
+        {
+            return a + b;
+        }
+       public static int vurma(int a, int b)
+        {
+                return a* b;
+        }
+
+    }
+}
